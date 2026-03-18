460248ce4e11f9420301f7599689e5612b08f412
Yuteng Lin <swaggyplyt@gmail.com>
2026-03-18 15:28:13 -0500

    Add customer management features including index, alphabetized, and missing email views; enhance styles and layout for improved UI

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,11 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all   
+  end
+  def alphabetized
+    @customers = Customer.order(:full_name).all
+  end
+  def missing_email
+    @customers = Customer.where(email: [nil, ""])
+  end
+end
